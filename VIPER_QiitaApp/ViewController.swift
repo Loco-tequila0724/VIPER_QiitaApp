29bb4bec11f20547fb7c48d7f36153b6694865a1
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-19 01:18:31 +0900

    Initial Commit

---
+++ VIPER_QiitaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER_QiitaApp
+//
+//  Created by 日高隼人 on 2023/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VIPER_QiitaAppUITests/VIPER_QiitaAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VIPER_QiitaAppUITestsLaunchTests.swift
+//  VIPER_QiitaAppUITests
+//
+//  Created by 日高隼人 on 2023/04/19.
+//
+
+import XCTest
+
+final class VIPER_QiitaAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
