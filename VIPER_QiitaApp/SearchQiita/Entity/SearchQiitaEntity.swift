cee2942d9590d9acbe29f5fa75c871df814e8672
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-19 21:23:07 +0900

    add: Qiitaのデータ構造 記述

--- VIPER_QiitaApp/SearchQiita/Entity/SearchQiitaEntity.swift
+++ VIPER_QiitaApp/SearchQiita/Entity/SearchQiitaEntity.swift
@@ -1,6 +1,21 @@
-
 import Foundation
 
-struct QiitaEntity {
+struct QiitaEntity: Codable {
+    let title: String
+    let url: String
+    let user: User
 
+    enum CodingKeys: CodingKey {
+        case title
+        case url
+        case user
+    }
+
+    struct User: Codable {
+        let profileImageURL: String
+
+        enum CodingKeys: String, CodingKey { // swiftlint:disable:this all
+            case profileImageURL = "profile_image_url"
+        }
+    }
 }
