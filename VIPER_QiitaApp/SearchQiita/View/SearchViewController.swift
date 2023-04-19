08714aad3250ab20ce03819a10c53e2ec6253064
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-19 20:20:06 +0900

    add:各UIパーツをVCへ定義

--- VIPER_QiitaApp/SearchQiita/View/SearchViewController.swift
+++ VIPER_QiitaApp/SearchQiita/View/SearchViewController.swift
@@ -1,21 +1,32 @@
 import UIKit
 
 class SearchQiitaViewController: UIViewController {
-    @IBOutlet weak var titleLabel: UILabel!{
-        didSet{
-            titleLabel.text = "俺的、VIPERアーキテクチャ"
-        }
-    }
-    @IBOutlet weak var searchInfoLabel: UILabel!{
-        didSet{
-            self.searchInfoLabel.text = "Qiita記事を検索🐍"
-        }
-    }
-
-    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var searchBar: UISearchBar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
 
+extension SearchQiitaViewController {
+    @IBAction private func showQiitaList(_ sender: Any) {
+    }
+}
+
+extension SearchQiitaViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension SearchQiitaViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
