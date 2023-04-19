ef3c221d463e74b92ec31f8106ee9145379ab262
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-19 20:20:40 +0900

    add: 細かな設定

--- VIPER_QiitaApp/SearchQiita/Entity/SearchQiitaEntity.swift
+++ VIPER_QiitaApp/SearchQiita/Entity/SearchQiitaEntity.swift
@@ -1,8 +1,6 @@
-//
-//  SearchQiitaEntity.swift
-//  VIPER_QiitaApp
-//
-//  Created by 日高隼人 on 2023/04/19.
-//
 
 import Foundation
+
+struct QiitaEntity {
+
+}
--- VIPER_QiitaApp/SearchQiita/Interactor/SearchQiitaInteractor.swift
+++ VIPER_QiitaApp/SearchQiita/Interactor/SearchQiitaInteractor.swift
@@ -1,8 +1,1 @@
-//
-//  SearchQiitaInteractor.swift
-//  VIPER_QiitaApp
-//
-//  Created by 日高隼人 on 2023/04/19.
-//
-
 import Foundation
