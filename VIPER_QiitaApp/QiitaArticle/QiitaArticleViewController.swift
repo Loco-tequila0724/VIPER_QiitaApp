78567d1598de326a1df8f408b014d6c8d054c41e
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-21 00:47:35 +0900

    add: ストーリボードインスタンス化 関数実装

--- VIPER_QiitaApp/QiitaArticle/QiitaArticleViewController.swift
+++ VIPER_QiitaApp/QiitaArticle/QiitaArticleViewController.swift
@@ -1,7 +1,16 @@
 import UIKit
+import WebKit
 
 class QiitaArticleViewController: UIViewController {
     var presenter: QiitaArticlePresentation?
+    static let storyboardID = "QiitaArticleID"
+
+    static func instantiate() -> UIViewController {
+        let storyboard = UIStoryboard(name: "QiiaArticle", bundle: Bundle.main)
+        guard let view = storyboard.instantiateViewController(withIdentifier: storyboardID) as? QiitaArticleViewController else {
+            return QiitaArticleViewController() }
+        return view
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -11,6 +20,5 @@ class QiitaArticleViewController: UIViewController {
 
 extension QiitaArticleViewController: QiitaArticleView {
     func configure() {
-
     }
 }
