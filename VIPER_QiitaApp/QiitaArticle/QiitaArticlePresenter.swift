24e8aa1f616b7bb825d4d562412d98e131973b7d
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-21 00:48:25 +0900

    add: プロバティ定義 , init定義

--- VIPER_QiitaApp/QiitaArticle/QiitaArticlePresenter.swift
+++ VIPER_QiitaApp/QiitaArticle/QiitaArticlePresenter.swift
@@ -2,12 +2,22 @@ import Foundation
 
 class QiitaArticlePresenter {
     var view: QiitaArticleView?
-    var presenter: QiitaArticlePresentation?
     var router: QiitaArticleWireFrame?
+    var qiitaArticle: QiitaEntity?
+
+    init(
+        view: QiitaArticleView? = nil,
+        router: QiitaArticleWireFrame? = nil,
+        qiitaArticle: QiitaEntity? = nil) {
+        self.view = view
+        self.router = router
+        self.qiitaArticle = qiitaArticle
+    }
 }
 
 extension QiitaArticlePresenter: QiitaArticlePresentation {
     func viewDidLoad() {
+        view?.configure()
     }
 
     func backButtonDidTapped() {
