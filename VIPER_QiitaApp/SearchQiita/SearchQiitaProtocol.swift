f72ac31f01a1413d28fea063b035fb13493c27fe
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-20 18:13:15 +0900

    add: プロトコルの定義 いろいろ変更

--- VIPER_QiitaApp/SearchQiita/SearchQiitaProtocol.swift
+++ VIPER_QiitaApp/SearchQiita/SearchQiitaProtocol.swift
@@ -3,6 +3,9 @@ import UIKit
 protocol SearchQiitaView: AnyObject {
     var presenter: SearchQiitaPresentation? { get set }
     func tableViewReload(qiitaList: [QiitaEntity?])
+    func configure()
+    func startLoading()
+    func stopLoading()
 }
 // Pesenter
 protocol SearchQiitaPresentation: AnyObject {
@@ -10,7 +13,7 @@ protocol SearchQiitaPresentation: AnyObject {
     var interactor: SearchQiitaInputUsecase? { get set }
     var router: SearchQiitaWireFrame? { get set }
     func viewDidLoad()
-    ///  検索ボタンタップ通知。
+    ///  検索ボタンタップ通知 。
     func searchButtonDidTapped(text: String)
     ///  セルタップ通知。
     func didSelectRow(qiitaArticle: QiitaEntity)
@@ -19,46 +22,17 @@ protocol SearchQiitaPresentation: AnyObject {
 protocol SearchQiitaInputUsecase: AnyObject {
     var presenter: SearchQiitaOutputUsecase? { get set }
     /// データベースからQiita記事(JSON)を取得
-    func fetchQiitaArticle(searchText: String) async -> (Result<[QiitaEntity?], ApiError>)
+    func fetchQiitaArticle(searchText: String) async
+    /// 取得したデータをQiita記事へと変換
+    func convertQiitaList()
 }
 // Interactor アウトプット
 protocol SearchQiitaOutputUsecase: AnyObject {
-    func didFetchQiitaArticle(qiitaList: [QiitaEntity])
+    /// Qiita記事をinteractorから取り → Viewへ渡す
+    func didFetchQiitaResult(qiitaList: [QiitaEntity])
 }
 // Router
 protocol SearchQiitaWireFrame: AnyObject {
-    static func assembleModuls() -> UIViewController
+    static func assembleModules() -> UIViewController
     func showQiitaWeb(qiitaURL: String)
 }
-// swiftlint:disable all
-// View
-//protocol TodoListView: AnyObject {
-//    var presenter: TodoListPresentation? { get set }
-//    func loadTodoList(_ todo: [TodoListEntity])
-//}
-//
-//// Interactor input
-//protocol TodoListInputUsecase: AnyObject {
-//    var output: TodoListOutputUsecase? { get set }
-//    func fetchTodoList()
-//}
-//
-//// Interactor output
-//protocol TodoListOutputUsecase: AnyObject {
-//    func loadTodoList(_ todoList: [TodoListEntity])
-//}
-//
-//// Presenter
-//protocol TodoListPresentation: AnyObject {
-//    var view: TodoListView? { get set }
-//    var interactor: TodoListInputUsecase { get set }
-//    var router: TodoListWireFrame { get set }
-//    func viewWillAppear()
-//    func didSelectRow(_ tapedRowIndex: Int)
-//}
-//
-//// Router
-//protocol TodoListWireFrame: AnyObject {
-//    static func assembleModules() -> UIViewController
-//    func showDetailVC(_ tappedRowIndex: Int)
-//}
