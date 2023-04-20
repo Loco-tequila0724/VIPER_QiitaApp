b08c2058b35b34c1a629ad8d7bccf2cde8fdef1d
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-20 23:57:39 +0900

    add: プロトコルを定義

---
+++ VIPER_QiitaApp/QiitaArticle/QiitaArticleProtocol.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+protocol QiitaArticleView {
+    var presenter: QiitaArticlePresentation? { get set }
+    func configure()
+}
+
+protocol QiitaArticlePresentation {
+    var view: QiitaArticleView? { get set }
+    var presenter: QiitaArticlePresentation? { get set }
+    var router: QiitaArticleWireFrame? { get set }
+    func viewDidLoad()
+    func backButtonDidTapped()
+}
+
+protocol QiitaArticleWireFrame {
+    static func assembleModules() -> UIViewController
+}
