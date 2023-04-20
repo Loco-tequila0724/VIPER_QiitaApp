7f9876b7e6e9981afbb71314568094eb50a29b57
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-20 18:09:15 +0900

    add: Qiita記事の取得や変換のメソッド。中身変更。

--- VIPER_QiitaApp/SearchQiita/Interactor/SearchQiitaInteractor.swift
+++ VIPER_QiitaApp/SearchQiita/Interactor/SearchQiitaInteractor.swift
@@ -3,12 +3,28 @@ import Foundation
 final class SearchQiitaInteractor {
     weak var presenter: SearchQiitaOutputUsecase?
     private let decoder = JSONDecoder()
+    private var qiitaRepository: (Result<[QiitaEntity?], ApiError>)?
 }
 
 extension SearchQiitaInteractor: SearchQiitaInputUsecase {
-    func fetchQiitaArticle(searchText: String) async -> (Result<[QiitaEntity?], ApiError>) {
+    ///  取得したQiitaの結果を返す
+    func convertQiitaList() {
+        guard let qiitaRepository else { return }
+        switch qiitaRepository {
+        case .success(let qiitaList):
+            let qiitaList = qiitaList.compactMap { $0 }
+            print("🙆‍♂️",qiitaList.count)
+            print(qiitaList.map { $0.user.profileImageURL })
+            presenter?.didFetchQiitaResult(qiitaList: qiitaList)
+        case .failure(let apiError):
+            print(apiError.localizedDescription)
+        }
+    }
+    /// API通信。データベースよりQiita記事の取得
+    func fetchQiitaArticle(searchText: String) async {
         guard let url: URL = URL(string: "https://qiita.com/api/v2/items?page=1&per_page=20&query=\(searchText)") else {
-            return .failure(.notFound)
+            qiitaRepository = .failure(.notFound)
+            return
         }
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -16,11 +32,14 @@ extension SearchQiitaInteractor: SearchQiitaInputUsecase {
         do {
             let (data, response) = try await URLSession.shared.data(for: request)
             guard let httpResponse = response as? HTTPURLResponse,
-                httpResponse.statusCode == 200 else { return .failure(.serverError) }
+                httpResponse.statusCode == 200 else {
+                qiitaRepository = .failure(.serverError)
+                return
+            }
             let qiitaList = try decoder.decode([QiitaEntity].self, from: data)
-            return .success(qiitaList)
+            qiitaRepository = .success(qiitaList)
         } catch {
-            return .failure(.networkError)
+            qiitaRepository = .failure(.networkError)
         }
     }
 }
